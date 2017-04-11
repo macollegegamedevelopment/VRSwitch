ff005e86f36f82f294791795d48bba68cf78c92c
Rens van Walstijn <r.walstijn@live.nl>
2017-04-11 07:55:05 +0100

    Added basic script

---
+++ Assets/DisableOnVREnabled.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class DisableOnVREnabled : MonoBehaviour 
+{
+	public MonoBehaviour[] components;
+
+	void Awake()
+	{
+		if (VRSettings.enabled)
+		{
+			foreach (var comp in components) 
+			{
+				comp.enabled = false;
+			}
+		}
+	}
+}
